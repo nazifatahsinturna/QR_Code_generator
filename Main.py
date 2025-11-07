6a8ab3889bd8efc4c8ce94ba0db1649f0f1ec98a
nazifatahsinturna <nazifatahsinturnabd@gmail.com>
2025-11-06 20:30:18 -0500

    customize feature add

--- Main.py
+++ Main.py
@@ -2,15 +2,18 @@ import qrcode
 
 #qr code generator function
 def generate_qr_code(text, filename, color): #function to genrate qrcode
-     qr = qrcode.ORCode (
-         version=1,
-         box_size=10,
-         border=5
-     )
-    # #qrcode make()
-    # image_qrcode = qrcode.make(text)
-    # #save the image
-    # image_qrcode.save(filename+".jpg")
+    qr = qrcode.QRCode (
+        version=1,
+        box_size=10,
+        border=5
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color=color, back_color='white')
+    img.save(filename+".png")
+
 
 
 text = input("Enter The text ot URL to convert into QR Code: ")
@@ -19,11 +22,11 @@ filename = input("Enter the filename to save the qrcode: ")
 color = ' '
 
 while(True): #letting the use choose
-    color = input("Pick a color (red, blue green, black): ")
+    color = input("Pick a color (red, blue, green, black): ")
 
     if color.lower() != 'red' and color.lower() != 'blue' and color.lower() != 'green' and color.lower() != 'black':
         print("Invalid Choice")
     else:
         break
 
-generate_qr_code(text, filename, colorhttps://github.com/nazifatahsinturna)
+generate_qr_code(text, filename, color)
